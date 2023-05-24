c8ebc5137d9ac8f8897388defd7c3421a9a969c3
Jose Paez <josefinapaezdevalle@gmail.com>
2023-05-24 19:17:05 -0300

    vue

---
+++ app.js
@@ -0,0 +1,36 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      title: 'CONTADOR',
+      descripcion: 'AGREGA ITEMS' ,
+      num: 0,
+      cantidad: 1,
+      operacion: 0,
+    },
+    methods: {
+      
+        aumentar(){
+            this.num += this.cantidad;
+            this.operacion = 1;
+        },
+        decrementar(){
+          this.num -= this.cantidad ;
+          this.operacion = 2;
+          console.log("vue decrementar");
+        },
+        resetear(){
+          this.num = 0;
+          this.operacion = 0;
+        }
+      },
+      computed: {
+         mostrarTituloOperacion() {
+           return this.operacion === 0 || this.num === 0 ?  "NEUTRO" : this.operacion === 1 ? " AGREGANDO" : " RESTANDO "
+        },
+        classObject() {
+          return this.operacion === 0 || this.num === 0 ?  "alert alert-primary" : this.operacion === 1 ? "alert alert-success" : "alert alert-danger"
+        }
+        
+      }
+  })
